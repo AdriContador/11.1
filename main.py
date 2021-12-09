e0809022ae28b71f0c4485ec3c030166b1b7e84e
AdriContador <acontador2@institutmvm.cat>
2021-12-09 12:40:33 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    parells = list()
+    for x in range(0,31):
+        if x % 2 == 0:
+            parells.append(x)
+    print(parells)
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
